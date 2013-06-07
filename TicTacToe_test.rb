2e56399f7838c19717466138fa351bf072daa1c3
wilbertcr <wilbertcr@gmail.com>
2013-06-07 17:18:49 -0600

    second commit

--- TicTacToe_test.rb
+++ TicTacToe_test.rb
@@ -2,8 +2,9 @@ require 'Tic_tac_toe.rb'
 
 tictactest = Tic_tac_toe.new()
 
-tictactest.display_board()
-tictactest.display_game_instructions()
-tictactest.whos_first?()
-tictactest.get_human_move()
-y
+
+#tictactest.display_board()
+#tictactest.display_game_instructions()
+#tictactest.whos_first?()
+#tictactest.get_human_move()
+puts "#{tictactest.check_winner([1,1,1,0,0,0,0,0,0])}"
--- Tic_tac_toe.rb
+++ Tic_tac_toe.rb
@@ -70,32 +70,69 @@ class Tic_tac_toe
     end
   end
   
-  def check_winner
-    
-  end
-  
-  def vertical_win
-    result = false
-    human_won = 1
-    machine_won = 1
-    
-    if (board[0]==board[3] && board[3]==board[6])
-      if board[6] == human_won
-        return human_won
-      else
-        return machine_won
+  def check_winner(board=@board)
+    checks_matrix = [vertical_win(board), horizontal_win(board), diagonal_win(board)]
+    puts "#{checks_matrix}"
+    winners = checks_matrix.rassoc(true)
+    if winners==nil
+      return false
+    else
+      return winners[0]
     end
-    
-    if (board[1]==board[4] && board[4]==board[7]) 
-    
-    
-    if (result)
-      
-    end 
-    
   end
-   
   
+  def vertical_win(board)
+    winner_not_found = false
+    if (board[0]==board[3] && board[3]==board[6] && board[6] != 0)
+      return winner?(board,6)
+    elsif (board[1]==board[4] && board[4]==board[7] && board[7] != 0)
+      return winner?(board,7)
+    elsif (board[2]==board[5] && board[5]==board[8] && board[8] != 0)
+      return winner?(board,8)
+    else
+      return [0,winner_not_found]
+    end
+  end  
+  
+  def horizontal_win(board)
+    winner_not_found = false
+    if (board[0]==board[1] && board[1]==board[2] && board[2] != 0)
+      puts "#{board[0]},#{board[1]},#{board[2]}"
+      return winner?(board,2)
+    elsif (board[3]==board[4] && board[4]==board[5] && board[5] != 0)
+      return winner?(board,5)
+    elsif (board[6]==board[7] && board[7]==board[8] && board[8] != 0)
+      return winner?(board,8)
+    else
+      return [0,winner_not_found]
+    end
+  end
+
+  def diagonal_win(board)
+    winner_not_found = false
+    if (board[0]==board[4] && board[4]==board[8] && board[8] != 0)
+      return winner?(board,8)
+    elsif (board[2]==board[4] && board[4]==board[6] && board[6] != 0)
+      return winner?(board,5)
+    else 
+      return [0,winner_not_found]
+    end
+  end  
+  
+  
+  def winner?(board,position)
+    human_won = 1
+    machine_won = 2
+    winner_found = true
+    winner_not_found = false
+    if board[position] == human_won
+      return [human_won,winner_found]
+    elsif board[position] == machine_won
+      return [machine_won,winner_found]
+    else
+      return[0,winner_not_found]
+    end
+  end
+  
+
 end
-
-
